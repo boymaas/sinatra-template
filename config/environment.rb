410d0a901333aa7aee2060cec99d24f20e45a6de
Boy Maas <boy.maas@gmail.com>
2010-09-18 23:16:53 +0200

    more restructuring

--- config/environment.rb
+++ config/environment.rb
@@ -9,8 +9,10 @@ require 'ostruct'
 
 require 'sinatra' unless defined?(Sinatra)
 
+# determine application's root directory
 ROOT_DIR = File.expand_path(File.join( File.dirname(__FILE__), '..'))
 
+# add libdirs to load path
 ['config', 'app/models', 'lib'].each do |relative_path|
   $LOAD_PATH.unshift(File.join(ROOT_DIR, relative_path))
 end
@@ -24,14 +26,14 @@ configure do
 
   ENVIRONMENT = Sinatra::Base.environment
 
-  # load models
+  # Load all defined models
   Dir.glob("#{ROOT_DIR}/app/models/*.rb") do |lib| 
     require File.basename(lib, '.*') 
   end
                
   DataMapper.setup(
     :default, 
-    (ENV["DATABASE_URL"] || "sqlite3:///#{ROOT_DIR}/db/#{ENVIRONMENT}.db")
+    (ENV["DATABASE_URL"] || "sqlite3:///#{ROOT_DIR}/db/#{ENVIRONMENT}.sqlite3")
   )
 
 end
